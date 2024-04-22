fc83b68acb6a80f541078a75bef797152943a3f7
Hardyn1 <amki.business@gmail.com>
2024-04-22 13:23:15 +0300

    arrays lesson start

---
+++ main.js
@@ -0,0 +1,24 @@
+// МАССИВЫ
+
+var student1 = "Aleksey Ivanov";
+		student2 = "Maria Ivanova";
+		student3 = "Denis Matvejev"
+
+// переменная students во множ. числе (так как много значений) [скобки] и все;
+var students = ["Aleksey Ivanov", "Maria Ivanova", "Denis Matvejev"];
+
+// Массивы нужно чтобы группировать какие то данные
+// (например: список учеников, название городов, ответы на тест).
+var colors = ["green", "red", "blue", "black"];
+var numbers = [245, 554, 139, 101];
+var answers = [true, false, true, false];
+
+// Массивы в основном начинают нумерацию с 0 (начинает счет с 0).
+var colors = ["green", "red", "blue", "black"];
+// green - 1 элемент массива colors, red - 2 элемент массива colors, и так далее.
+
+// Обращение к элементу массива (console.log(cities[2]) и выводится "narva").
+var cities = ["moscow", "tallinn", "narva", "praga", "sochi"];
+console.log(
+	cities[2]
+);
